01b009d12db5192a865c34017f64157e375c8990
Alan Rynne <alan@rynne.es>
2022-12-13 23:08:40 +0100

    Added logging

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const path = require("path")
 async function setup() {
   // Get version of tool to be installed
   const version = core.getInput("version")
+  console.log("Starting Yak setup with version:", version)
 
   const url = `https://files.mcneel.com/yak/tools/${version}/yak.exe`
-
-  // Download the specific version of the tool, e.g. as a tarball
   const pathToExe = await tc.downloadTool(url)
+
+  console.log("Downloaded yak into:", pathToExe)
+
   const cached = await tc.cacheFile(pathToExe, "yak.exe", "yak")
+
+  console.log("Cached yak as:", cached)
+
   // Expose the tool by adding it to the PATH
   core.addPath(path.dirname(cached))
+  console.log("Added yak dir to path", path.dirname(cached))
 }
 
 module.exports = setup
