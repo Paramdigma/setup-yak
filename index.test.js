bcb4b09da423fd57806f3daf27d91bac318afd43
Alan Rynne <alan@speckle.systems>
2022-12-14 18:29:11 +0100

    feat: added input token and output version

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ const setup = require("./index")
 test("Yak path is set by action", async () => {
   // Read action config from action.yml file.
   const target = RunTarget.asyncFn(setup, "action.yml")
-  const opts = RunOptions.create({ inputs: { version: "latest" } })
+  const opts = RunOptions.create()
   // Run it with default options
   var result = await target.run(opts)
 
@@ -16,9 +16,7 @@ test("Invalid version returns error", async () => {
   // Read action config from action.yml file.
   const target = RunTarget.asyncFn(setup, "action.yml")
   // Run it with default options
-  var result = await target.run(
-    RunOptions.create({ inputs: { version: "x.y" } })
-  )
+  var result = await target.run(RunOptions.create())
 
   expect(result.error).not.toBeNull()
   expect(result.commands.addedPaths).toHaveLength(0)
